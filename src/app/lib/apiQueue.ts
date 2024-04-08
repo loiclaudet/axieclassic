86a80fb23b5c7fee3588bfe9904d8d2807152f6d
loiclaudet <8344874+loiclaudet@users.noreply.github.com>
2024-04-08 18:53:31 +0200

    feat: add request queuing to deal with api limitation

---
+++ src/app/lib/apiQueue.ts
@@ -0,0 +1,8 @@
+import PQueue from "p-queue";
+import { X_RATELIMIT_LIMIT_SECOND } from "~/app/lib/constant";
+
+export const apiQueue = new PQueue({
+  concurrency: X_RATELIMIT_LIMIT_SECOND,
+  interval: 1000,
+  intervalCap: X_RATELIMIT_LIMIT_SECOND,
+});
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -5,56 +5,60 @@ import type {
   Battles,
   APIError,
 } from "~/app/lib/definitions";
+import { apiQueue } from "./apiQueue";
 
 export async function getBattles(
   clientId: string,
   options: APIOptions = {},
 ): Promise<Battles | APIError> {
-  const limit = options.limit ?? 10;
-  const offset = options.offset ?? 0;
-
-  try {
-    const response = await fetch(
-      `https://api-gateway.skymavis.com/classic/v1/community/users/${clientId}/battle-histories?limit=${limit}&offset=${offset}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "X-API-Key": env.X_API_KEY,
-        },
-        next: {
-          revalidate: 600, // revalidate every at most 10 minutes
-        },
-      },
-    );
+  const run = async () => {
+    const response = await fetchBattles(clientId, options);
 
     if (!response.ok) {
-      if (response.status === 429) {
-        // Handle rate limiting
-        return {
-          error: true,
-          status: 429,
-          message: "Rate limit exceeded. Please try again later.",
-        };
-      }
-      // Handle other errors
-      return {
-        error: true,
-        status: response.status,
-        message: "An error occurred while fetching battles.",
-      };
+      throw new Error(response.statusText);
     }
 
     const data = (await response.json()) as Battles;
     return data;
+  };
+
+  try {
+    const result = await apiQueue.add(run);
+
+    if (!result) {
+      throw new Error("No result");
+    }
+
+    return result;
   } catch (error) {
-    // Handle network or other unexpected errors
+    console.error(error);
     return {
       error: true,
-      status: 0,
-      message: "Network error or unexpected error occurred.",
+      status: 500,
+      message: "Error fetching battles.",
     };
   }
+}
+
+async function fetchBattles(clientId: string, options: APIOptions = {}) {
+  const limit = options.limit ?? 10;
+  const offset = options.offset ?? 0;
+
+  const response = await fetch(
+    `https://api-gateway.skymavis.com/classic/v1/community/users/${clientId}/battle-histories?limit=${limit}&offset=${offset}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": env.X_API_KEY,
+      },
+      next: {
+        revalidate: 600, // revalidate every at most 10 minutes
+      },
+    },
+  );
+
+  return response;
 }
 
 export async function getLeaderboard(options: APIOptions = {}) {
