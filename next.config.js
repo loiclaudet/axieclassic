c6d7a5adad8e2641deac260628dfb4d378f649aa
loiclaudet <8344874+loiclaudet@users.noreply.github.com>
2024-04-10 17:51:25 +0200

    chore: add full url fetch logging

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
