f583e75740266b5dc57bdd9f105398354be52a92
loiclaudet <8344874+loiclaudet@users.noreply.github.com>
2024-04-12 14:53:16 +0200

    fix: increase promise interval duration to avoid build error due to API limit

--- src/app/lib/apiQueue.ts
+++ src/app/lib/apiQueue.ts
@@ -3,6 +3,6 @@ import { X_RATELIMIT_LIMIT_SECOND } from "~/app/lib/constant";
 
 export const apiQueue = new PQueue({
   concurrency: X_RATELIMIT_LIMIT_SECOND,
-  interval: 1000,
+  interval: 1100,
   intervalCap: X_RATELIMIT_LIMIT_SECOND,
 });
