7795bc71b6a37e2275102960d346bf879b5e6da6
loiclaudet <8344874+loiclaudet@users.noreply.github.com>
2024-07-23 09:20:57 +0300

    feat: update leaderboard api

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,12 +1,19 @@
 import type {
-  Leaderboard,
+  RankedUsersResponse,
   APIOptions,
   Battles,
   APIError,
   Season,
+  ClientID,
+  ProfilesResponse,
+  Profile,
+  RankedUser,
+  Player,
 } from "~/app/lib/definitions";
 import { apiQueue } from "./apiQueue";
 import { getNextAPIKey } from "~/app/lib/apiKeys";
+import { MAXIMUM_PLAYERS_API_LIMIT } from "./constant";
+import { chunk } from "src/app/utils";
 
 export async function getSeason(): Promise<Season | APIError> {
   try {
@@ -76,14 +83,60 @@ async function fetchBattles(clientId: string, options: APIOptions = {}) {
   return response;
 }
 
-export async function getLeaderboard(options: APIOptions = {}) {
-  const limit = options.limit ?? 50;
-  const offset = options.offset ?? 0;
+export async function getPlayers(): Promise<Player[] | APIError> {
+  try {
+    const rankedUsers = (await getTop100RankedUsers()) as RankedUser[];
+    const profiles = (await getProfiles(
+      rankedUsers.map((user) => user.clientID),
+    )) as Profile[];
 
+    const players = profiles.map((profile, index) => ({
+      ...profile,
+      ...rankedUsers[index]!,
+    }));
+
+    return players;
+  } catch (error) {
+    console.error(error);
+    return {
+      error: true,
+      status: 500,
+      message: "Error fetching players.",
+    };
+  }
+}
+
+export async function getTop100RankedUsers(): Promise<RankedUser[] | APIError> {
+  // fetch top 100 ranked users
+  // there is no pagination for this endpoint :(
   const response = await fetch(
-    `https://axie-classic.skymavis.com/v1/leaderboards?limit=${limit}&offset=${offset}`,
+    `https://axie-classic.skymavis.com/v1/season/leaderboard`,
   );
 
-  const data = (await response.json()) as Leaderboard;
-  return data;
+  const data = (await response.json()) as RankedUsersResponse;
+  return data.items;
+}
+
+export async function getProfiles(
+  profileClientIDs: ClientID[],
+): Promise<Profile[] | APIError> {
+  const endpoints = chunk(profileClientIDs, MAXIMUM_PLAYERS_API_LIMIT).flatMap(
+    createProfilesEndpoint,
+  );
+
+  const profiles = (await Promise.all(endpoints.map(fetchProfiles))).flat();
+  return profiles;
+}
+
+async function fetchProfiles(profilesEndpoint: string) {
+  const response = await fetch(profilesEndpoint);
+  const data = (await response.json()) as ProfilesResponse;
+  return data.items;
+}
+
+function createProfilesEndpoint(clientIDs: ClientID[]): string {
+  return (
+    "https://axie-classic.skymavis.com/v1/players/profile?" +
+    clientIDs.map((p) => "clientIDs=" + p).join("&")
+  );
 }
